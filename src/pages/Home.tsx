c4195e743e8817610c2d8fb1c180083aad1cf954
dongwonkwon <ehddnjs0710@gmail.com>
2024-02-11 19:29:06 +0900

    Feat: home(로그인페이지)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,32 @@
-import Footer from "../components/Footer";
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column; /* 세로로 나열하기 위한 설정 */
+  align-items: center; /* 가로 중앙 정렬 */
+  justify-content: center; /* 세로 중앙 정렬 */
+  height: 100vh;
+`;
+
+const StyledImage = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  margin-bottom: 100px;
+`;
+
+const StyledButton = styled.img`
+  margin-top: 10px; /* 버튼 간격 조정 */
+`;
 
 export default  function Home() {
     return(
         <div>
-            <h1>Home</h1>
-            <p>가장 먼저 보이는 페이지. 로고. 소셜로그인.</p>
-            <Footer/>
+            <StyledContainer>
+                <StyledImage src={process.env.PUBLIC_URL + '/FfunLogo.png'} alt="Logo" />
+                <StyledButton src={process.env.PUBLIC_URL + '/NaverLogin.png'} alt="Naver"/>
+                <StyledButton src={process.env.PUBLIC_URL + '/KakaoLogin.png'} alt="Kakao" />
+                <StyledButton src={process.env.PUBLIC_URL + '/GoogleLogin.png'} alt="Google" />
+            </StyledContainer>
         </div>
     )
 }
